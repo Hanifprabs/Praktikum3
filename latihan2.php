e4d52d65b671153c73903132c8064e8fb1974a64
Hanif.Prabs <muhammadhanifprabowo@gmail.com>
2024-03-27 13:50:20 +0700

    tambah repository

---
+++ latihan2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $nim = '01202207010';
+    $nama = 'Muhammad Hanif Prabowo';
+    $alamat = 'Jl. Slamet riyadi No.26';
+    $nilai = 90;
+    echo "NIM : ", $nim, "<br>";
+    echo "Nama : ", $nama, "<br>";
+    echo "Alamat : ", $alamat, "<br>";
+    echo "Nilai : $nilai";
+?>
+</body>
+</html>
